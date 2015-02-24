d218dc4a98a06121ac459b8bfa26440e0a5b4ab0
Shane Vander Meer <shane.vander.meer@globalrelay.net>
2015-02-24 15:34:33 -0800

    blah

--- TailChaser/Code/Observer/ConcreteObserver.cs
+++ TailChaser/Code/Observer/ConcreteObserver.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace TailChaser.Code.Observer
 {
-    class ConcreteObserver
+    public class ConcreteObserver : Observer
     {
+        private readonly ConcreteSubject _subject;
+        private readonly string _name;
+
+        public ConcreteObserver(ConcreteSubject subject, string name)
+        {
+            _subject = subject;
+            _name = name;
+        }
+
+        public override void Update()
+        {
+            _subject.GetFileContent();
+        }
     }
 }
+//flowdocuments and rtfs. http://www.dofactory.com/net/observer-design-pattern
