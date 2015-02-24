88bb43e16651644f254079e05384d1cfd8985c87
Shane Vander Meer <shane.vander.meer@globalrelay.net>
2015-02-23 16:48:37 -0800

    Moving poco

---
+++ TailChaser.Tail/FileChange.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TailChaser.Tail
+{
+    public class FileChange
+    {
+        public string FilePath { get; set; }
+        public DateTime ChangeDetected { get; set; }
+    }
+}
