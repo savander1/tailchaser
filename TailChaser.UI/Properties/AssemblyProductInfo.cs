9d20d605f5e6bbe5cf5fb8c184def610d2ec1df8
Shane Vander Meer <shane.vander.meer@globalrelay.net>
2015-04-20 16:02:51 -0700

    adding version and product info

---
+++ TailChaser.UI/Properties/AssemblyProductInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+
+[assembly: AssemblyCompany("Shane Vander Meer")]
+[assembly: AssemblyProduct("TailChaser")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
--- TailChaser/Properties/AssemblyInfo.cs
+++ TailChaser/Properties/AssemblyInfo.cs
@@ -10,10 +10,6 @@ using System.Windows;
 [assembly: AssemblyTitle("TailChaser")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TailChaser")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -40,16 +36,3 @@ using System.Windows;
     // app, or any theme specific resource dictionaries)
 )]
 
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
