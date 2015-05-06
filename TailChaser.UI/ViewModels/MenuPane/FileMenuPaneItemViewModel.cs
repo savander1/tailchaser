b1c7f0679c975db86d8d14e28ac6436abf5954c3
Shane Vander Meer <shane.vander.meer@globalrelay.net>
2015-05-06 16:19:14 -0700

    Something is showing up!

--- TailChaser.UI/ViewModels/MenuPane/FileMenuPaneItemViewModel.cs
+++ TailChaser.UI/ViewModels/MenuPane/FileMenuPaneItemViewModel.cs
@@ -6,6 +6,7 @@ namespace TailChaser.UI.ViewModels.MenuPane
     {
         public FileMenuPaneItemViewModel(MainWindowViewModel parent) : base(parent)
         {
+            Name = "File";
             var saveItem = new SaveCommandViewModel(null, null);
             var closeItem = new CloseCommandViewModel(x => ((MainWindowViewModel)Parent).Close(), null);
             AddCommands(saveItem, closeItem);
